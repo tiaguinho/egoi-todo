20f308f2c61139dd63528c6fdd9a22b41c80054f
Tiago Temporin <tiaguinhocab@gmail.com>
2019-04-10 18:16:52 +0100

    feat: add edit and list components

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -10,10 +10,12 @@ import {
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
 import { TodoService } from './todo.service';
 
 @NgModule({
-  declarations: [AddTodoComponent],
+  declarations: [AddTodoComponent, ListTodoComponent, EditTodoComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -25,6 +27,6 @@ import { TodoService } from './todo.service';
     MatFormFieldModule,
   ],
   providers: [TodoService],
-  exports: [AddTodoComponent],
+  exports: [AddTodoComponent, EditTodoComponent, ListTodoComponent],
 })
 export class TodoModule {}
