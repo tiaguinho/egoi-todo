6ba573a1ac9cdcb962359bae1c02662264c47421
Tiago Temporin <tiaguinhocab@gmail.com>
2019-04-10 18:15:35 +0100

    refactor: add errors message

--- src/app/todo/add-todo/add-todo.component.ts
+++ src/app/todo/add-todo/add-todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { priorities } from '../todo.interface';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'add-todo',
@@ -12,13 +13,31 @@ export class AddTodoComponent implements OnInit {
 
   public priorities = priorities;
 
-  constructor(private fb: FormBuilder) {}
+  get priority() {
+    return this.addForm.get('priority');
+  }
+  get title() {
+    return this.addForm.get('title');
+  }
+  get description() {
+    return this.addForm.get('description');
+  }
+
+  constructor(private fb: FormBuilder, private todo: TodoService) {}
 
   ngOnInit() {
     this.addForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(5)]],
+      priority: ['', Validators.required],
+      title: ['', [Validators.minLength(5), Validators.required]],
       description: ['', Validators.required],
       done: [false],
     });
+  }
+
+  submit({ value, valid }): void {
+    if (valid) {
+      this.todo.add(value);
+      this.addForm.reset();
+    }
   }
 }
