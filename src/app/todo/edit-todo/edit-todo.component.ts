d2d1b7bb5cb71ad2bb70f5b856298c362a9df5f5
Tiago Temporin <tiaguinhocab@gmail.com>
2019-04-15 18:03:48 +0100

    feat: implement edit form

--- src/app/todo/edit-todo/edit-todo.component.ts
+++ src/app/todo/edit-todo/edit-todo.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { priorities } from '../todo.interface';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'edit-todo',
   templateUrl: './edit-todo.component.html',
   styleUrls: ['./edit-todo.component.scss'],
 })
-export class EditTodoComponent implements OnChanges {
+export class EditTodoComponent implements OnInit, OnChanges {
   @Input('id')
   set selectedID(id: number) {
     this._selecteID = id;
@@ -15,9 +18,43 @@ export class EditTodoComponent implements OnChanges {
   }
   private _selecteID: number;
 
-  constructor() {}
+  public priorities = priorities;
+
+  get priority() {
+    return this.editForm.get('priority');
+  }
+  get title() {
+    return this.editForm.get('title');
+  }
+  get description() {
+    return this.editForm.get('description');
+  }
+
+  editForm: FormGroup;
+
+  constructor(private todo: TodoService, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.editForm = this.fb.group({
+      id: [''],
+      priority: ['', Validators.required],
+      title: ['', [Validators.minLength(5), Validators.required]],
+      description: ['', Validators.required],
+      done: [false],
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
+    if (changes['selectedID'].currentValue) {
+      const todo = this.todo.getTodo(this._selecteID);
+      this.editForm.setValue(todo);
+    }
+  }
+
+  submit({ value, valid }): void {
+    if (valid) {
+      this.todo.edit(this._selecteID, value);
+      this.editForm.reset();
+    }
   }
 }
