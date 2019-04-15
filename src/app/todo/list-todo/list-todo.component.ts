f81eed1f14bc49a9a6263a0bfda4f47c742cf3bf
Tiago Temporin <tiaguinhocab@gmail.com>
2019-04-15 18:04:13 +0100

    feat: implement list of todos

--- src/app/todo/list-todo/list-todo.component.ts
+++ src/app/todo/list-todo/list-todo.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+// import { Subscription } from 'rxjs';
 import { Todo } from '../todo.interface';
 import { TodoService } from '../todo.service';
 
@@ -7,23 +9,40 @@ import { TodoService } from '../todo.service';
   templateUrl: './list-todo.component.html',
   styleUrls: ['./list-todo.component.scss'],
 })
-export class ListTodoComponent implements OnInit {
+export class ListTodoComponent implements OnInit, OnDestroy {
+  /*
   todos: Todo[] = [];
+  private subscription: Subscription;
+  */
+
+  todos$: Observable<Todo[]>;
 
   @Output() onEditClick = new EventEmitter();
 
   constructor(private todo: TodoService) {}
 
   ngOnInit() {
-    this.todos = this.todo.list();
+    //this.subscription = this.todo.list().subscribe(items => (this.todos = items));
+    this.todos$ = this.todo.list();
+  }
+
+  ngOnDestroy() {
+    /*
+    if (this.subscription && !this.subscription.closed) {
+      this.subscription.unsubscribe();
+    }
+    */
   }
 
   remove(id: number): void {
     this.todo.remove(id);
-    this.todos = this.todo.list();
   }
 
   edit(id: number): void {
     this.onEditClick.emit(id);
+  }
+
+  markAsDone(id: number): void {
+    this.todo.edit(id, { done: true });
   }
 }
