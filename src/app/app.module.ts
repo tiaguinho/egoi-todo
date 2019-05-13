91a8adf85eb788da4329258b63cb408ec778c501
Tiago Temporin <tiaguinhocab@gmail.com>
2019-05-13 18:15:02 +0100

    feat: http module and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountService } from './account/account.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TodoModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
+  providers: [AccountService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
